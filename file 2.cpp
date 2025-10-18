996fa819a9b06b0d090e903042ae68dab9f6b895
zctwss8vf7-ux <zctwss8vf7@privaterelay.appleid.com>
2025-10-18 12:22:24 +0200

    Add files via upload

---
+++ file 2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Long Double: " << sizeof(long double) << endl;
+    cout << "Double: " << sizeof(double) << endl;
+    cout << "Float: " << sizeof(float) << endl;
+    cout << "Long Long: " << sizeof(long long) << endl;
+    cout << "Long: " << sizeof(long) << endl;
+    cout << "Int: " << sizeof(int) << endl;
+    cout << "Short: " << sizeof(short) << endl;
+    cout << "Char: " << sizeof(char) << endl;
+    cout << "Bool: " << sizeof(bool) << endl;
+    cout << "String: " << sizeof(string) << endl;
+    return 0;
+}
